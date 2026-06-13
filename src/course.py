6c417a9d25edfc2e52ba7eefbfd2b31959f87342
Цветинка Хаджиева <cvetinkah05@gmail.com>
2026-06-13 19:39:22 +0300

    Add Course model

---
+++ src/course.py
@@ -0,0 +1,14 @@
+class Course:
+    def __init__(self, name):
+        self.name = name
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def remove_task(self, task):
+        if task in self.tasks:
+            self.tasks.remove(task)
+
+    def get_tasks_count(self):
+        return len(self.tasks)
