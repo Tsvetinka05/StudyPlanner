2c7989385df534b5c222094cd6a1b55d232d2202
Цветинка Хаджиева <cvetinkah05@gmail.com>
2026-06-14 10:17:58 +0300

    Connect planner components

---
+++ main.py
@@ -0,0 +1,35 @@
+from src.task import Task
+from src.course import Course
+from src.planner import StudyPlanner
+
+planner = StudyPlanner()
+
+python_course = Course("Python")
+
+task1 = Task(
+    "Finish homework",
+    "Python",
+    90,
+    1,
+    "2026-06-20"
+)
+
+task2 = Task(
+    "Study OOP",
+    "Python",
+    120,
+    2,
+    "2026-06-25"
+)
+
+task1.mark_completed()
+
+python_course.add_task(task1)
+python_course.add_task(task2)
+
+planner.add_course(python_course)
+
+print("Courses:", planner.get_courses_count())
+print("Total tasks:", planner.get_total_tasks())
+print("Completed tasks:", planner.get_completed_tasks())
+print("Pending tasks:", planner.get_pending_tasks())
