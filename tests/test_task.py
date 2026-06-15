5592b2a0ecc9593decd693527f8fd3a5044cae6f
Цветинка Хаджиева <cvetinkah05@gmail.com>
2026-06-15 21:23:49 +0300

    Add task completion test

--- tests/test_task.py
+++ tests/test_task.py
@@ -14,3 +14,16 @@ def test_task_creation():
     assert task.subject == "Python"
     assert task.estimated_minutes == 60
     assert task.is_completed == False
+
+def test_mark_completed():
+    task = Task(
+        "Homework",
+        "Python",
+        60,
+        1,
+        "2026-06-20"
+    )
+
+    task.mark_completed()
+
+    assert task.is_completed == True
