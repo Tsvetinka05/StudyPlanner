72323b3cbe62e3c1dd56ae794da889d8b036ce32
Цветинка Хаджиева <cvetinkah05@gmail.com>
2026-06-26 22:11:19 +0300

    Add task input validation and messages

--- src/gui.py
+++ src/gui.py
@@ -1,13 +1,15 @@
 import tkinter as tk
 from tkinter import messagebox
+from datetime import datetime
+
 from src.task import Task
 from src.database import Database
-from datetime import datetime
+
 
 def start_gui():
     window = tk.Tk()
     window.title("Study Planner")
-    window.geometry("500x500")
+    window.geometry("600x600")
 
     tasks = []
 
@@ -25,8 +27,40 @@ def start_gui():
     def add_task():
         task_text = task_entry.get()
         subject = subject_entry.get()
-        estimated_time = int(time_entry.get())
+        time_text = time_entry.get()
         deadline = deadline_entry.get()
+
+        if task_text == "" or task_text == "Task name":
+            messagebox.showerror("Task Error", "Please enter a task name.")
+            return
+
+        if subject == "" or subject == "Subject":
+            messagebox.showerror("Task Error", "Please enter a subject.")
+            return
+
+        if time_text == "" or time_text == "Estimated minutes":
+            messagebox.showerror("Task Error", "Please enter the estimated study time.")
+            return
+
+        if not time_text.isdigit():
+            messagebox.showerror("Task Error", "Estimated study time must contain only numbers.")
+            return
+
+        estimated_time = int(time_text)
+
+        if estimated_time <= 0:
+            messagebox.showerror("Task Error", "Estimated study time must be greater than 0.")
+            return
+
+        if deadline == "" or deadline == "Deadline":
+            messagebox.showerror("Task Error", "Please enter a deadline.")
+            return
+
+        try:
+            datetime.strptime(deadline, "%Y-%m-%d")
+        except ValueError:
+            messagebox.showerror("Task Error", "Deadline must be in format YYYY-MM-DD.")
+            return
 
         task = Task(
             task_text,
@@ -35,60 +69,6 @@ def start_gui():
             1,
             deadline
         )
-
-        if task_text == "" or task_text == "Task name":
-            messagebox.showwarning(
-            "Warning",
-            "Please enter a task name."
-            )
-            return
-    
-        if subject == "" or subject == "Subject":
-            messagebox.showwarning(
-                "Warning",
-                "Please enter a subject."
-            )
-            return
-    
-        if estimated_time == "" or estimated_time == "Estimated minutes":
-            messagebox.showwarning(
-                "Warning",
-                "Please enter estimated minutes."
-            )
-            return
-
-        if not estimated_time.isdigit():
-            messagebox.showwarning(
-                "Warning",
-                "Estimated minutes must be a number."
-            )
-            return
-
-        estimated_time = int(estimated_time)
-
-        if estimated_time <= 0:
-            messagebox.showwarning(
-                "Warning",
-                "Estimated minutes must be greater than 0."
-            )
-            return
-
-        
-        if deadline == "" or deadline == "Deadline":
-            messagebox.showwarning(
-                "Warning",
-                "Please enter a deadline."
-            )
-            return
-
-        try:
-            datetime.strptime(deadline, "%Y-%m-%d")
-        except ValueError:
-            messagebox.showwarning(
-                "Warning",
-                "Deadline must be in format YYYY-MM-DD."
-            )
-            return
 
         database.add_task(task)
 
@@ -102,6 +82,29 @@ def start_gui():
         time_entry.delete(0, tk.END)
         deadline_entry.delete(0, tk.END)
 
+        add_placeholder(task_entry, "Task name")
+        add_placeholder(subject_entry, "Subject")
+        add_placeholder(time_entry, "Estimated minutes")
+        add_placeholder(deadline_entry, "Deadline")
+
+        messagebox.showinfo("Success", "Task added successfully!")
+
+    def complete_task():
+        selected_task = task_listbox.curselection()
+
+        if not selected_task:
+            messagebox.showwarning("Warning", "Please select a task.")
+            return
+
+        index = selected_task[0]
+        task_text = tasks[index]
+
+        if "completed" not in task_text:
+            tasks[index] = task_text + " - completed"
+
+        task_listbox.delete(index)
+        task_listbox.insert(index, tasks[index])
+
     def delete_task():
         selected_task = task_listbox.curselection()
 
@@ -113,21 +116,6 @@ def start_gui():
 
         task_listbox.delete(index)
         tasks.pop(index)
-    
-    def complete_task():
-        selected_task = task_listbox.curselection()
-
-        if not selected_task:
-            messagebox.showwarning("Warning", "Please select a task.")
-            return
-
-        index = selected_task[0]
-        task_text = tasks[index]
-
-        tasks[index] = task_text + " - completed"
-
-        task_listbox.delete(index)
-        task_listbox.insert(index, tasks[index])
 
     def show_statistics():
         completed = 0
@@ -143,7 +131,7 @@ def start_gui():
             f"Total tasks: {len(tasks)}\n"
             f"Completed: {completed}\n"
             f"Pending: {pending}"
-    )
+        )
 
     title = tk.Label(
         window,
@@ -152,26 +140,25 @@ def start_gui():
     )
     title.pack(pady=20)
 
-    
-    task_entry = tk.Entry(window, width=30)
-    task_entry.pack(pady=10)
+    task_entry = tk.Entry(window, width=40)
+    task_entry.pack(pady=5)
     task_entry.insert(0, "Task name")
     task_entry.bind("<FocusIn>", lambda event: clear_placeholder(task_entry, "Task name"))
     task_entry.bind("<FocusOut>", lambda event: add_placeholder(task_entry, "Task name"))
 
-    subject_entry = tk.Entry(window, width=30)
+    subject_entry = tk.Entry(window, width=40)
     subject_entry.pack(pady=5)
     subject_entry.insert(0, "Subject")
     subject_entry.bind("<FocusIn>", lambda event: clear_placeholder(subject_entry, "Subject"))
     subject_entry.bind("<FocusOut>", lambda event: add_placeholder(subject_entry, "Subject"))
 
-    time_entry = tk.Entry(window, width=30)
+    time_entry = tk.Entry(window, width=40)
     time_entry.pack(pady=5)
     time_entry.insert(0, "Estimated minutes")
     time_entry.bind("<FocusIn>", lambda event: clear_placeholder(time_entry, "Estimated minutes"))
     time_entry.bind("<FocusOut>", lambda event: add_placeholder(time_entry, "Estimated minutes"))
 
-    deadline_entry = tk.Entry(window, width=30)
+    deadline_entry = tk.Entry(window, width=40)
     deadline_entry.pack(pady=5)
     deadline_entry.insert(0, "Deadline")
     deadline_entry.bind("<FocusIn>", lambda event: clear_placeholder(deadline_entry, "Deadline"))
@@ -182,39 +169,43 @@ def start_gui():
         text="Add Task",
         command=add_task
     )
-    add_task_button.pack(pady=10)
+    add_task_button.pack(pady=8)
+
     complete_task_button = tk.Button(
-    window,
-    text="Complete Task",
-    command=complete_task
+        window,
+        text="Complete Task",
+        command=complete_task
     )
-    complete_task_button.pack(pady=10)
+    complete_task_button.pack(pady=8)
 
     delete_task_button = tk.Button(
-    window,
-    text="Delete Task",
-    command=delete_task
-        )
-
-    delete_task_button.pack(pady=10)
+        window,
+        text="Delete Task",
+        command=delete_task
+    )
+    delete_task_button.pack(pady=8)
 
     statistics_button = tk.Button(
         window,
         text="Show Statistics",
         command=show_statistics
     )
-    statistics_button.pack(pady=10)
+    statistics_button.pack(pady=8)
 
-    task_listbox = tk.Listbox(window, width=50)
-    
+    task_listbox = tk.Listbox(window, width=70)
+    task_listbox.pack(pady=20)
 
     saved_tasks = database.get_tasks()
 
     for task in saved_tasks:
         task_title = task[1]
-        tasks.append(task_title)
-        task_listbox.insert(tk.END, task_title)
-    
-    task_listbox.pack(pady=20)
+        task_subject = task[2]
+        task_time = task[3]
+        task_deadline = task[5]
+
+        task_display = f"{task_title} | {task_subject} | {task_time} min | {task_deadline}"
+
+        tasks.append(task_display)
+        task_listbox.insert(tk.END, task_display)
 
     window.mainloop()
